8c8a90583d24e5377ae8ee5bd5acfc65e3ca22de
Prasanth Louis <prasanthlouis21@gmail.com>
2015-09-08 11:29:57 -0400

    Second Commit

---
+++ app/src/androidTest/java/com/example/kaitpicco/puzzleapplication/MainActivityUnitTest.java
@@ -0,0 +1,143 @@
+package com.example.kaitpicco.puzzleapplication;
+
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kaitpicco on 9/7/15.
+ */
+public class MainActivityUnitTest extends ActivityUnitTestCase<MainActivity> {
+    Intent mLaunchIntent;
+    public MainActivityUnitTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+
+
+
+
+    }
+
+  @SmallTest
+  public void testgetVar()
+  {
+      MainActivity ma=new MainActivity();
+      assertEquals(false, ma.getvar());
+  }
+    @SmallTest
+    public void testmainact()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+    }
+
+    @SmallTest
+    public void teststringchecker()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        String s=ma.stringjoiner("Hello","There");
+        assertEquals("Hello There", s);
+    }
+
+    @SmallTest
+    public void teststringchecker2()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        String s=ma.stringjoiner("","There");
+        assertEquals(" There", s);
+    }
+
+    @SmallTest
+      public void teststringchecker3()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        String s=ma.stringjoiner("Hello","");
+        assertEquals("Hello ", s);
+    }
+
+    @SmallTest
+    public void teststringchecker4()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        String s=ma.stringjoiner("","");
+        assertEquals(" ", s);
+    }
+
+
+
+    @SmallTest
+    public void teststringchecker5()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        ma.setVar(1);
+        String s=ma.stringjoiner("Hello","There");
+        assertEquals("There Hello", s);
+    }
+
+    @SmallTest
+    public void teststringchecker6()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        ma.setVar(1);
+        String s=ma.stringjoiner("","There");
+        assertEquals("There ", s);
+    }
+
+    @SmallTest
+    public void teststringchecker7()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        ma.setVar(1);
+        String s=ma.stringjoiner("Hello","");
+        assertEquals(" Hello", s);
+    }
+
+    @SmallTest
+    public void teststringchecker8()
+    {
+        mLaunchIntent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
+        startActivity(mLaunchIntent, null, null);
+        MainActivityFragment ma=new MainActivityFragment();
+        ma.setVar(1);
+        String s=ma.stringjoiner("","");
+        assertEquals(" ", s);
+    }
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
---
+++ app/src/main/java/com/example/kaitpicco/puzzleapplication/Behaviour.java
@@ -0,0 +1,8 @@
+package com.example.kaitpicco.puzzleapplication;
+
+/**
+ * Created by kaitpicco on 9/7/15.
+ */
+public interface Behaviour {
+    public void paneData(String s);
+}
